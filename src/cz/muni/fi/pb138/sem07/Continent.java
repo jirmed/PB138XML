6e4502475d3b06703ab1c0b5854b14e1ed24334b
Jiri Medved <medved@volny.cz>
2018-02-25 17:02:34 +0100

    getLargestCity

--- src/cz/muni/fi/pb138/sem07/Continent.java
+++ src/cz/muni/fi/pb138/sem07/Continent.java
@@ -6,22 +6,46 @@
 package cz.muni.fi.pb138.sem07;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  *
  * @author jirka
  */
-public class Continent extends Part  {
+public class Continent extends Part {
 
-   
     Continent(Element continentElement) {
         this.partElement = continentElement;
     }
 
-    
     @Override
     public String getName() {
         return partElement.getAttribute("name");
     }
-    
+
+    City getLargestCity() {
+        City result = null;
+        City[] cities = getCities();
+        for (City city : cities) {
+            if (result == null || result.getPopulation() < city.getPopulation()) {
+                result = city;
+            }
+
+        }
+        return result;
+    }
+
+    City[] getCities() {
+        City[] cities = null;
+        NodeList citiesNl = partElement.getElementsByTagName("city");
+        if (!citiesNl.equals(null)) {
+            int citiesCount = citiesNl.getLength();
+            cities = new City[citiesCount];
+            for (int i = 0; i < citiesCount; i++) {
+                cities[i] = new City((Element) citiesNl.item(i));
+            }
+        }
+        return cities;
+    }
+
 }
--- test/cz/muni/fi/pb138/sem07/ContinentTest.java
+++ test/cz/muni/fi/pb138/sem07/ContinentTest.java
@@ -7,19 +7,20 @@ package cz.muni.fi.pb138.sem07;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import org.junit.Test;
 import org.w3c.dom.Element;
 
 /**
  *
  * @author jirka
  */
-public class ContinentTest extends PartTestAbstract{
+public class ContinentTest extends PartTestAbstract {
 
     private Continent continent;
 
     @Override
     public void setUp() {
-        continent = new Continent((Element)document.getElementsByTagName("continent").item(0));
+        continent = new Continent((Element) document.getElementsByTagName("continent").item(0));
     }
 
     @Override
@@ -27,13 +28,27 @@ public class ContinentTest extends PartTestAbstract{
     }
 
     @Override
+    @Test
     public void testGetNodeName() {
         assertThat(continent.getNodeName(), is("continent"));
     }
 
     @Override
+    @Test
     public void testGetName() {
         assertThat(continent.getName(), is("asia"));
     }
-    
+
+    @Test
+    public void testGetCities() {
+        City[] cities = continent.getCities();
+        assertThat(cities.length, is(7));
+
+    }
+
+    @Test 
+    public void testGetLargestCity() {
+      City largestCity = continent.getLargestCity();
+      assertThat(largestCity.getName(),is("Shangai"));
+}
 }
